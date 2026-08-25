a78ad281da3ab6a40bd2450360e4bd65d2a79b3e
Hachimaki-dev <hachimakidev@gmail.com>
2026-08-25 11:11:15 -0400

    Hoy vimos cursores simples y cursores con parametro

--- Semana 3/cursores.sql
+++ Semana 3/cursores.sql
@@ -48,3 +48,49 @@ INNER JOIN RESERVA_TEMPORAL rt ON c.CLIENTE_ID = rt.CLIENTE_ID
 INNER JOIN TRANSACCION_PAGO tp ON tp.RESERVA_ID = rt.CLIENTE_ID
 WHERE tp.ESTADO = 'APROBADO' ;
 
+--Generar un reporte con todas las transacciones aprobadas
+DECLARE 
+    CURSOR c_transacciones_aprobadas IS 
+        SELECT c.NOMBRE , C.RUT, rt.ESTADO AS ESTADO_DE_LA_RESERVA , tp.MONTO_BRUTO, tp.DESCUENTO, tp.MONTO_FINAL, tp.ESTADO AS ESTADO_DE_LA_TRANSACCION  FROM CLIENTE c 
+        INNER JOIN RESERVA_TEMPORAL rt ON c.CLIENTE_ID = rt.CLIENTE_ID
+        INNER JOIN TRANSACCION_PAGO tp ON tp.RESERVA_ID = rt.CLIENTE_ID
+        WHERE tp.ESTADO = 'APROBADO' ;
+BEGIN
+
+    FOR por_cada_transaccion IN c_transacciones_aprobadas LOOP 
+        DBMS_OUTPUT.PUT_LINE('********REPORTE TRANSACCIONES APROBADAS*********');
+        DBMS_OUTPUT.PUT_LINE('Nombre ' || por_cada_transaccion.NOMBRE);
+        DBMS_OUTPUT.PUT_LINE('RUT ' || por_cada_transaccion.RUT);
+        DBMS_OUTPUT.PUT_LINE('Monto final ' || por_cada_transaccion.MONTO_FINAL);
+        DBMS_OUTPUT.PUT_LINE('Estado Transacción ' || por_cada_transaccion.ESTADO_DE_LA_TRANSACCION);
+    END LOOP;
+    null; 
+END;
+/
+
+--CURSOR CON PARAMETROS - CURSORES COMPLEJOS
+--Antes de, insertemos unos varguitas
+
+INSERT INTO CLIENTE(RUT, NOMBRE, APELLIDO, EMAIL, TELEFONO, FECHA_REGISTRO) VALUES ('11.111.111-1', 'Fernandito', 'Vargas', 'fvargas@gmail.com', '+5611111111', SYSTIMESTAMP);
+
+INSERT INTO CLIENTE(RUT, NOMBRE, APELLIDO, EMAIL, TELEFONO, FECHA_REGISTRO) VALUES ('22.222.222-2', 'Luciana', 'Vargas', 'lvargas@gmail.com', '+56922222222', SYSTIMESTAMP);
+
+INSERT INTO CLIENTE(RUT, NOMBRE, APELLIDO, EMAIL, TELEFONO, FECHA_REGISTRO) VALUES ('33.333.333-6', 'Ricardito', 'Vargas', 'rvargas@gmail.com', '+5633333333',  SYSTIMESTAMP);
+COMMIT;
+SELECT * FROM CLIENTE;
+
+DECLARE
+    CURSOR c_clientes_por_apellido(p_apellido VARCHAR2) IS
+            SELECT cliente_id, nombre, apellido, email
+            FROM CLIENTE
+            WHERE APELLIDO = p_apellido;
+
+BEGIN
+    FOR un_cliente IN c_clientes_por_apellido('Vargas')
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(
+            un_cliente.nombre
+        );
+    END LOOP;
+END;
+/
